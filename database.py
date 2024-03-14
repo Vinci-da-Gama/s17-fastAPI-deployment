b4e614f9726dab625435f7b5635b0165edd65995
BoaHankok <boahankok@BoaHankoks-MacBook-Pro.local>
2024-03-15 10:17:35 +1100

    give elephonesql

--- database.py
+++ database.py
@@ -4,10 +4,18 @@ from sqlalchemy.orm import sessionmaker, registry
 # Define a registry
 mapper_registry = registry()
 
+"""
 SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+"""
+SQLALCHEMY_DATABASE_URL = ("postgresql://dgvlrrpa:Zkew136ffOTwBF00i_gTzfAwvI-uPowH@rosie"
+                           ".db.elephantsql.com/dgvlrrpa")
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL
 )
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
